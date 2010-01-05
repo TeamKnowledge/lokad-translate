ef405d49d43daca05b7a5bf1c3a48194e0edc2af
joannes.vermorel <devnull@localhost>
2010-01-05 08:42:52 +0000

    Fix to match Azure port forwarding behavior.

--- Source/Lokad.Translate/BusinessLogic/AuthorizeOrRedirectAttribute.cs
+++ Source/Lokad.Translate/BusinessLogic/AuthorizeOrRedirectAttribute.cs
@@ -16,9 +16,16 @@ namespace Lokad.Translate.BusinessLogic
 
 			if (filterContext.Result is HttpUnauthorizedResult)
 			{
-				filterContext.Result = 
-					new RedirectResult("~/Account/Login?returnUrl=" + 
-						HttpUtility.UrlEncode(filterContext.RequestContext.HttpContext.Request.Url.ToString()));
+				// do not use 'filterContext.RequestContext.HttpContext.Request.Url' because of Azure port forwarding
+				// http://social.msdn.microsoft.com/Forums/en-US/windowsazure/thread/9142db8d-0f85-47a2-91f7-418bb5a0c675/
+
+				var scheme = filterContext.RequestContext.HttpContext.Request.Url.Scheme;
+				var host = filterContext.RequestContext.HttpContext.Request.Headers["Host"];
+				var path = filterContext.RequestContext.HttpContext.Request.RawUrl;
+				var returnUrl = scheme + @"://" + host + path;
+
+				filterContext.Result =
+					new RedirectResult("~/Account/Login?returnUrl=" + HttpUtility.UrlEncode(returnUrl));
 			}
 		}
 	}
